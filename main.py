320a224d42e1fd6c18533376cbe023fffe17a3fe
Michael Clinckett <80383953+michaelclinckett@users.noreply.github.com>
2022-02-08 23:31:46 +0000

    idk

--- main.py
+++ main.py
@@ -2,12 +2,73 @@
 Source: https://www.askpython.com/python-modules/tkinter/age-calculator
 Date: 22/01/2022
 '''
-#__________ Functions __________#
+#__________ IMPORT __________#
+from datetime import date
+from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox
+today = date.today()
 
+#__________ Functions __________#
+def get_age():
+    #get the 3 entries
+    d = int(e_date.get())
+    m = int(e_month.get())
+    y = int(e_year.get())
+
+    calc_age = find_age(d, m, y)
+    display_calc_age(calc_age)
+
+def find_age(d, m, y):
+    #find the age 
+    age = today.year-y-((today.month, today.day)<(m,d))
+    tbox_age.config(state= 'normal')
+
+def display_calc_age(age):
+    tbox_age.config(state='normal')
+    #age calculated is insert into the text box after clearing th eprevious info in the textbox.
+    tbox_age.delete('1.0', tk.END)
+    tbox_age.insert(tk.END,age)
+    tbox_age.config(state='disabled')
+
+def exit():
+    window.destroy()
+
+def validation():
+  #gets the three entries
+  d = e_date.get()
+  m = e_month.get()
+  y = e_year.get()
+
+
+  msg = ''
+  
+  if len(d) == 0 or len(m) == 0 or len(y) == 0:
+    msg = 'day, month and year can\'t be empty'
+  else:
+    try:
+      if any(ch.isdigit() for ch in d) == False:
+        
+        msg = 'day must be a number idiot'
+      elif any(ch.isdigit() for ch in m) == False:
+        msg = 'month must be a number idiot'
+      elif any(ch.isdigit() for ch in y) == False:
+        msg = 'year must be a number idiot'
+      else:
+      # msg = 'success!'
+        day = int(d)
+        month = int(m)
+        year = int(y)
+        calc_age = find_age(day, month, year)
+        display_calc_age(calc_age)
+  
+    except Exception as ep:
+      messagebox.showerror('error', ep)
+  messagebox.showerror('message', msg)
+  
 
 #__________ Main __________#
 
-import tkinter as tk
 
 window = tk.Tk()
 window.geometry("400x300")
@@ -41,7 +102,7 @@ e_month = tk.Entry(window,width=5)
 e_year = tk.Entry(window,width=5)
 
 #Button to calculate age
-btn_calculate_age = tk.Button(window,text="Calculate Age",font=("Arial",13), command='get_age')
+btn_calculate_age = tk.Button(window,text="Calculate Age",font=("Arial",13), command= validation)
 
 #label for text box that will display the calculated age
 lb_calculated_age = tk.Label(window,text="The calculated age is: ",
@@ -65,19 +126,7 @@ lb_calculated_age.place(x=10,y=200)
 tbox_age.place(x=240,y=200)
 btn_exit.place(x=100,y=240)
 
-def get_age():
-    #get the 3 entries
-    d = int(e_date.get())
-    m = int(e_month.get())
-    y = int(e_year.get())
 
-    #find the age 
-    age = today.year-y-((today.month, today.day)<(m,d))
-    tbox_age.config(state= 'normal')
-
-    #age calculated is insert into the text box after clearing th eprevious info in the textbox.
-    tbox_age.delete('1.0', tk.END)
-    tbox_age.
 
 
 
